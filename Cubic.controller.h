dc3d7e35556801dfa7a96f648569ed7d4b752b00
Hiroto Konishi <hirokoni.hirokoni@gmail.com>
2023-02-14 21:10:51 +0900

    Add documentation

--- Cubic.controller.h
+++ Cubic.controller.h
@@ -5,14 +5,32 @@
 
 namespace Cubic_controller
 {
+    /**
+     * @brief エンコーダの種類を示します
+     *
+     * @param inc incremental encoder
+     * @param abs absolute encoder
+     *
+     */
     enum class encoderType
     {
         inc,
         abs
     };
 
+    /**
+     * @brief エンコーダを読みます
+     *
+     * @param encoderNo
+     * @param encoderType
+     * @return int32_t 値
+     */
     int32_t readEncoder(int encoderNo, enum encoderType encoderType);
 
+    /**
+     * @brief 速度制御を行うためのクラスです。
+     *
+     */
     class Velocity_PID
     {
     private:
@@ -35,6 +53,10 @@ namespace Cubic_controller
         int getDuty() const;
     };
 
+    /**
+     * @brief 位置制御を行うためのクラスです。
+     *
+     */
     class Position_PID
     {
     private:
@@ -63,29 +85,61 @@ namespace Cubic_controller
         int32_t angleToEncoder(double angle, int32_t currentEncoder) const;
     };
 
+    /**
+     * @brief 目標速度を設定します。
+     *
+     * @param target エンコーダのカウント値/秒
+     */
     inline void Velocity_PID::setTarget(const double target)
     {
         this->pid->setTarget(target);
     }
+    /**
+     * @brief PIDゲインを（再）設定します。
+     *
+     * @param Kp
+     * @param Ki
+     * @param Kd
+     */
     inline void Velocity_PID::setGains(const double Kp, const double Ki, const double Kd)
     {
         this->pid->setGains(Kp, Ki, Kd);
     }
+    /**
+     * @brief 目標速度を取得します。
+     *
+     * @return double
+     */
     inline double Velocity_PID::getTarget() const
     {
         return this->pid->getTarget();
     }
+    /**
+     * @brief Duty比を取得します。内部で計算は行われないので、マルチスレッドでもない限り使用は想定していません。
+     *
+     * @return int
+     */
     inline int Velocity_PID::getDuty() const
     {
         return this->duty;
     }
 
+    /**
+     * @brief 目標角度を設定します。
+     *
+     * @param target [deg] 0~360
+     */
     inline void Position_PID::setTarget(const double target)
     {
         this->targetAngle = target;
         this->pid->setTarget(target);
     }
 
+    /**
+     * @brief 目標角度を現在の角度に対して相対的に設定します。
+     *
+     * @param target [deg]
+     */
     inline void Position_PID::setTargetByRelative(const double target)
     {
         // get current angle and add target
@@ -93,19 +147,41 @@ namespace Cubic_controller
         this->pid->setTarget(this->targetAngle);
     }
 
+    /**
+     * @brief PIDゲインを（再）設定します。
+     *
+     * @param Kp
+     * @param Ki
+     * @param Kd
+     */
     inline void Position_PID::setGains(const double Kp, const double Ki, const double Kd)
     {
         this->pid->setGains(Kp, Ki, Kd);
     }
+    /**
+     * @brief 目標角度を取得します。
+     *
+     * @return double [deg]
+     */
     inline double Position_PID::getTarget() const
     {
         return this->targetAngle;
     }
+    /**
+     * @brief Duty比を取得します。内部で計算は行われないので、マルチスレッドでもない限り使用は想定していません。
+     *
+     * @return int
+     */
     inline int Position_PID::getDuty() const
     {
         return this->duty;
     }
-
+    /**
+     * @brief エンコーダの値を角度に変換します。
+     *
+     * @param encoder
+     * @return double [deg] 0~360
+     */
     inline double Position_PID::encoderToAngle(const int32_t encoder) const
     {
         double angle = encoder * 360.0 / (double)CPR;
@@ -119,6 +195,13 @@ namespace Cubic_controller
         }
         return angle;
     }
+    /**
+     * @brief 角度をエンコーダの値に変換します。
+     *
+     * @param angle [deg] 0~360
+     * @param currentEncoder 現在のエンコーダ値。
+     * @return int32_t 現在のエンコーダ値に最も近い、angleを示す値を返します。
+     */
     inline int32_t Position_PID::angleToEncoder(const double angle, const int32_t currentEncoder) const
     {
         int32_t encoder = base + (angle * CPR) / 360.0;
--- Readme.md
+++ Readme.md
@@ -4,5 +4,13 @@
 
 Cubic 2.0に依存しています。
 
+## Usage
+
+`Cubic_controller::Velocity_PID` により速度制御を行います。`target`は、目標速度です。（エンコーダのカウント値/秒）
+
+`Cubic_controller::Position_PID` により位置制御を行います。`target`は、目標位置です。（コンストラクタ呼び出し時を0とした角度[deg]）
+
+それぞれ、内部で`DC_motor::put()`しています。
+
 ## Update history
 
