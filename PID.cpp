0af67cab2736057385d652a467b0f8e1d5118a22
Konishi, Hiroto <hirokoni.hirokoni@gmail.com>
2023-02-19 15:20:48 +0900

    fix bug where duty is limited to 255

--- PID.cpp
+++ PID.cpp
@@ -2,56 +2,58 @@
 
 namespace PID
 {
-PID::PID(int capableDuty, double Kp, double Ki, double Kd,double current, double target, bool direction)
-: Kp(Kp), Ki(Ki), Kd(Kd), capableDuty(abs(limitInPermitedDutyRange(capableDuty))), current(current), target(target), direction(direction){
-  preMicros = micros();
-  preDiff = 0;
-  integral = 0;
-}
-
-int PID::compute_PID(double current, const bool logging){
-
-  /* Update dt */
-  unsigned long nowMicros = micros();
-  if (nowMicros < preMicros)
+  PID::PID(int16_t capableDuty, double Kp, double Ki, double Kd, double current, double target, bool direction)
+      : Kp(Kp), Ki(Ki), Kd(Kd), capableDuty(capableDuty), current(current), target(target), direction(direction)
   {
-    dt = MAX_MICROSECONDS - preMicros + nowMicros;
+    preMicros = micros();
+    preDiff = 0;
+    integral = 0;
   }
-  else
+
+  int16_t PID::compute_PID(double current, const bool logging)
   {
-    dt = nowMicros - preMicros;
+
+    /* Update dt */
+    unsigned long nowMicros = micros();
+    if (nowMicros < preMicros)
+    {
+      dt = MAX_MICROSECONDS - preMicros + nowMicros;
+    }
+    else
+    {
+      dt = nowMicros - preMicros;
+    }
+    dt /= 1000000.0;
+    preMicros = nowMicros;
+
+    this->current = current;
+    diff = target - current;
+
+    if (!direction)
+    {
+      diff *= -1.0;
+    }
+
+    /* Compute duty */
+    integral += (diff + preDiff) * dt / 2.0;
+    duty = Kp * diff + Ki * integral + Kd * (diff - preDiff) / dt;
+
+    duty = dutyLimiter();
+
+    preDiff = diff;
+
+    if (logging)
+    {
+      Serial.print("current:");
+      Serial.print(current);
+      Serial.print(",target:");
+      Serial.print(target);
+      Serial.print(",diff:");
+      Serial.print(diff);
+      Serial.print(",duty:");
+      Serial.println(duty);
+    }
+    return duty;
   }
-  dt /= 1000000.0;
-  preMicros = nowMicros;
-
-  this->current = current;
-  diff = target - current;
-
-
-  if(!direction){
-  diff *= -1.0;
-  }
-
-  /* Compute duty */
-  integral += (diff + preDiff) * dt / 2.0;
-  duty = Kp * diff + Ki * integral + Kd * (diff - preDiff) / dt;
-
-  duty = dutyLimiter();
-
-  preDiff = diff;
-
-  if (logging)
-  {
-    Serial.print("current:");
-    Serial.print(current);
-    Serial.print(",target:");
-    Serial.print(target);
-    Serial.print(",diff:");
-    Serial.print(diff);
-    Serial.print(",duty:");
-    Serial.println(duty);
-  }
-  return duty;
-}
 
 }
