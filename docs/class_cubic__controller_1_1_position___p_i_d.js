337dea28a9501566e52453b65b6f36b4a9e08c6c
Konishi, Hiroto <hirokoni.hirokoni@gmail.com>
2023-02-20 15:35:17 +0900

    regenerate document

--- docs/class_cubic__controller_1_1_position___p_i_d.js
+++ docs/class_cubic__controller_1_1_position___p_i_d.js
@@ -2,7 +2,7 @@ var class_cubic__controller_1_1_position___p_i_d =
 [
     [ "Position_PID", "class_cubic__controller_1_1_position___p_i_d.html#a1fef265a641360c33c183d655ce0884d", null ],
     [ "encoderToAngle", "class_cubic__controller_1_1_position___p_i_d.html#a1e011dbd19dacf636cd97259f1d69b4d", null ],
-    [ "getDuty", "class_cubic__controller_1_1_position___p_i_d.html#ad7b803986dff6629dd1bcc6cb355b4fd", null ],
+    [ "getDuty", "class_cubic__controller_1_1_position___p_i_d.html#aa09a8e9889ec611ab028a8420d18ceaa", null ],
     [ "getTarget", "class_cubic__controller_1_1_position___p_i_d.html#a421e0fe90368f21e7d9384e7a63572a7", null ],
     [ "setGains", "class_cubic__controller_1_1_position___p_i_d.html#a880d120021781abbc78c1eb36fda85ed", null ],
     [ "setKd", "class_cubic__controller_1_1_position___p_i_d.html#a1b8b861b9e6d1e70a59266d533f0d562", null ],
--- docs/class_cubic__controller_1_1_velocity___p_i_d.js
+++ docs/class_cubic__controller_1_1_velocity___p_i_d.js
@@ -1,7 +1,7 @@
 var class_cubic__controller_1_1_velocity___p_i_d =
 [
     [ "Velocity_PID", "class_cubic__controller_1_1_velocity___p_i_d.html#ab6cc2f668f63e1a6c9a65665b64e6034", null ],
-    [ "getDuty", "class_cubic__controller_1_1_velocity___p_i_d.html#aa76537670656f09f9cf98d88b9fb8d45", null ],
+    [ "getDuty", "class_cubic__controller_1_1_velocity___p_i_d.html#ac78693de722a02613cff4c95d2f0cbcc", null ],
     [ "getTarget", "class_cubic__controller_1_1_velocity___p_i_d.html#aa52fd289b302b51efdc6b7c6a9790dfc", null ],
     [ "setGains", "class_cubic__controller_1_1_velocity___p_i_d.html#a427e016f32b43e61f5c4b7daf53de704", null ],
     [ "setKd", "class_cubic__controller_1_1_velocity___p_i_d.html#a9822c7ff0482c9bcc302a756b35920c7", null ],
