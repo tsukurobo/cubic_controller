f369a100c291af09d0293f8260578f6dbbfaa625
Konishi, Hiroto <39662862+HiroKoni@users.noreply.github.com>
2023-04-22 22:53:00 +0900

    generate docs

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['encodertoangle_0',['encoderToAngle',['../class_cubic__controller_1_1_controller.html#a832aa55747b1d59fba432f840c1d494d',1,'Cubic_controller::Controller::encoderToAngle()'],['../_cubic_8controller_8h.html#af6ef91d8809b03ea7cbc16a0bc71fc06',1,'Cubic_controller::encoderToAngle()']]]
+  ['encodertoangle_0',['encoderToAngle',['../class_cubic__controller_1_1_controller.html#a64dc9bc03afa2fa17adc7a75382df044',1,'Cubic_controller::Controller::encoderToAngle()'],['../class_cubic__controller_1_1_velocity___p_i_d.html#ac03c33386886f95cf22dd0bda38dfbfa',1,'Cubic_controller::Velocity_PID::encoderToAngle()'],['../class_cubic__controller_1_1_position___p_i_d.html#a9c355c96f2349f3eb53bec2979cc4b77',1,'Cubic_controller::Position_PID::encoderToAngle()'],['../_cubic_8controller_8h.html#acf0f20437952cab65e5762c612251dbb',1,'Cubic_controller::encoderToAngle()']]]
 ];
