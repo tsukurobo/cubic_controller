e9af3a7942c1193617a9cdf4fab3d94fc9b08532
Neko2234 <99799346+Neko2234@users.noreply.github.com>
2024-02-09 16:15:09 +0900

    サンプルコードのB面対応＆リファクタリング

--- cubic_controller.ino
+++ cubic_controller.ino
@@ -6,16 +6,28 @@
 
 void setup()
 {
-  Cubic::begin();
+  Cubic::begin(true);
   Serial.begin(115200);
 }
 
 void loop()
 {
-  using namespace Cubic_controller;
-  static Velocity_PID velocityPID(0, 0, encoderType::inc, 2048 * 4, 0.5, 0.0, 0.0, 4.0, false, 1.0, 0.8, true);
-  static Position_PID positionPID(1, 0, encoderType::abs, AMT22_CPR, 0.5, 0.0, 0.0, degToRad(90.0), false, 1.0, true);
+  // using namespace Cubic_controller;
+
+  uint8_t motorNo = 13; uint8_t encoderNo = 0;
+  uint16_t CPR = 2048 * 4; // エンコーダのCPR(カウント/1回転) = PPR(パルス/1回転)
+  double Kp = 4.0; double Ki = 0.0; double Kd = 0.0; // PIDゲイン
+  double velTarget = 4.0; double posTarget = degToRad(90.0); // 目標角速度[rad/s]、目標角度[rad] (-PI<= target < PI)
+  bool direction = true; // モータが正回転したときにエンコーダの値が+方向に増えるならtrue
+  double capableDutyCycle = 0.5; // 最大許容デューティ比。0.0~1.0。省略可能で、デフォルトは1.0。
+  double lowpassFilter = 1.0; // ローパスフィルタの係数。0.0~1.0。省略可能で、デフォルトは1.0(フィルタなし)。
+  bool logging = true; // ログをSerial.printで出力するかどうか。省略可能で、デフォルトはfalse。
+
+  static Cubic_controller::Velocity_PID velocityPID(motorNo, encoderNo, Cubic_controller::encoderType::inc, CPR, Kp, Ki, Kd, velTarget, direction, capableDutyCycle, lowpassFilter, logging);
+  static Cubic_controller::Position_PID positionPID(motorNo, encoderNo, Cubic_controller::encoderType::abs, Cubic_controller::AMT22_CPR, Kp, Ki, Kd, posTarget, direction, capableDutyCycle, logging);
+
   static bool stopFlag = false;
+  static bool confirmFlag = false;
   if (Serial.available() > 0)
   {
     char c = Serial.read();
@@ -49,6 +61,10 @@ void loop()
       velocityPID.reset();
       positionPID.reset();
     }
+    else if (c == 'c')
+    {
+      confirmFlag = true;
+    }
     else
     {
       stopFlag = true;
@@ -57,15 +73,27 @@ void loop()
   if (stopFlag)
   {
     Serial.println("stopping...");
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 23; i++)
     {
       DC_motor::put(i, 0);
     }
   }
+  else if (confirmFlag)
+  {
+    // モータの回転方向確認()
+    for (int i = 0; i < 23; i++)
+    {
+      DC_motor::put(i, 100);
+    }
+    Serial.print("Inc: ");
+    Inc_enc::print();
+    Serial.print(", Abs: ");
+    Abs_enc::print(true);
+  }
   else
   {
-    // velocityPID.compute();
-    positionPID.compute();
+    velocityPID.compute();
+    // positionPID.compute();
   }
   Cubic::update();
 }
